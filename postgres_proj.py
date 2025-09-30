f8ea6e761482d9c0439c3e48994f991b9a2244e1
abdulhameed <abdulhameed00815@gmail.com>
2025-09-30 03:16:50 +0300

    even more progress in pg

--- postgres_proj.py
+++ postgres_proj.py
@@ -5,7 +5,7 @@ conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", pa
 cur = conn.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS person(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY, 
     name VARCHAR(255),
     age INT CHECK (age > 0),
     gender CHAR
@@ -16,7 +16,8 @@ cur.execute("""INSERT INTO person (id, name, age, gender) VALUES
 (1, 'mohammed', 65, 'm'),
 (2, 'basmalla', 21, 'f'),
 (3, 'ronaldo', 43, 'm'),
-(4, 'sobhy', 23, 'm');
+(4, 'sobhy', 23, 'm')
+ON CONFLICT (id) DO NOTHING;
 """)
 
 cur.execute("""CREATE TABLE IF NOT EXISTS boys(
@@ -24,15 +25,38 @@ cur.execute("""CREATE TABLE IF NOT EXISTS boys(
     boy_id INT REFERENCES person(id)
 )""")
 
-cur.execute("""INSERT INTO boys (id, boy_id) VALUES
-(1, 1)
+cur.execute("""INSERT INTO boys (boy_id) VALUES (1)
+ON CONFLICT (id) DO NOTHING;
 """)
 
-cur.execute("""SELECT boys.boy_id, person.name, person.age, person.gender
-            FROM boys
-            JOIN person ON boys.boy_id = person.id;
+cur.execute("""CREATE TABLE IF NOT EXISTS orders(
+    id SERIAL PRIMARY KEY,
+    person_id INT REFERENCES person(id),
+    item TEXT
+    
+);
+""")
+
+cur.execute("""INSERT INTO orders (person_id, item) VALUES 
+(1, 'book'),
+(2, 'laptop'),
+(3, 'phone');
+""")
+
+cur.execute("""SELECT boys.id AS boys_row, person.name, orders.item
+FROM boys
+JOIN person ON boys.boy_id = person.id
+JOIN orders ON person.id = orders.person_id;
 """)
 print(cur.fetchall())
+
+#cur.execute("""SELECT boys.boy_id, person.name, person.age, person.gender
+ #           FROM boys
+  #          FULL OUTER JOIN person ON boys.boy_id = person.id;
+#""")
+#print(cur.fetchall())
+
+
 
 
 conn.commit()
