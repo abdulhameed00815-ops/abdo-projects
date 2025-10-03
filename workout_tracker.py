f2561ee0b763b00560aaba3c5c720a0443b85cf9
abdulhameed <abdulhameed00815@gmail.com>
2025-10-03 22:01:48 +0300

    another commit for the project

--- workout_tracker.py
+++ workout_tracker.py
@@ -5,28 +5,40 @@ cur = conn.cursor()
 
 
 
-def add_workout():
-    cur.execute("""CREATE TABLE IF NOT EXISTS workouts(
+cur.execute("""CREATE TABLE IF NOT EXISTS workouts(
         workout_type VARCHAR(255),
         workout_time INT,
         workout_date DATE
     );
 """)
-    cur.execute("""INSERT INTO workouts (workout_type, workout_time, workout_date) VALUES (%s,%s, CURRENT_DATE)
-""", (added_workout, 30))
+conn.commit()
 
+    
 
 def view_workouts():
     cur.execute("""SELECT *
         FROM workouts
 """)
     print(cur.fetchall())
-choice = input(": ")
-if choice == "s":
-    print(view_workouts())
-elif choice == "a":
-    add = add_workout()
-    print(add)
+
+while True:
+    choice = input("""
+'s': show workouts
+'a': add workout
+:
+""")
+    if choice == "s":
+        print(view_workouts())
+    elif choice == "a":
+        added_workout = input("workout: ")
+        cur.execute("""INSERT INTO workouts (workout_type, workout_time, workout_date) VALUES (%s, %s, CURRENT_DATE);
+    """, (added_workout, 30))
+        print("workout added successfuly!")
+
+cur.close()
+conn.close()
+
+
 
 
 
