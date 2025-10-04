72a859e2d782ade5c76a275252548bf05c1dc910
abdulhameed <abdulhameed00815@gmail.com>
2025-10-04 18:36:29 +0300

    got alot of things done

--- workout_tracker.py
+++ workout_tracker.py
@@ -11,60 +11,73 @@ cur.execute("""CREATE TABLE IF NOT EXISTS workouts(
         workout_date DATE
     );
 """)
-conn.commit()
 
-    
 
-def view_workouts():
-    cur.execute("""SELECT *
-        FROM workouts
+def add_workout():
+    added_workout = input("workout: ")
+    cur.execute("""INSERT INTO workouts (workout_type, workout_time, workout_date) VALUES (%s, %s, CURRENT_DATE);
+        """, (added_workout, 30))
+    print("workout added successfuly!")
+
+def weekly_workouts():
+    cur.execute("""SELECT * FROM workouts WHERE workout_date >= CURRENT_DATE - INTERVAL '7 days';
+""")
+    print(cur.fetchall())
+
+def time_trained():
+    cur.execute("""SELECT SUM(workouts.workout_time) AS total_time FROM workouts;
 """)
     print(cur.fetchall())
 
 while True:
     choice = input("""
-'s': show workouts
+'s': show all-time workouts
 'a': add workout
+'q': quit
+'w': show week workouts
+'t': show all-time time spent training (seconds)
 :
 """)
     if choice == "s":
-        print(view_workouts())
+        cur.execute("SELECT * FROM workouts;")
+        print(cur.fetchall())
     elif choice == "a":
-        added_workout = input("workout: ")
-        cur.execute("""INSERT INTO workouts (workout_type, workout_time, workout_date) VALUES (%s, %s, CURRENT_DATE);
-    """, (added_workout, 30))
-        print("workout added successfuly!")
-
-cur.close()
-conn.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        add_workout()
+    elif choice == "w":
+        weekly_workouts()
+    elif choice == 't':
+        time_trained()
+    elif choice == "q":
+        break
 
 
 conn.commit()
-
 cur.close()
-
 conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
