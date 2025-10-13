e6a9954621068bea86ab8925a336f438aae55bba
abdo <abdulhameed00815@gmail.com>
2025-10-13 05:44:10 -0400

    sqlalchemy basic code

---
+++ sqlalchemy_proj.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+engine = create_engine("postgresql+psycopg2://postgres:1234@localhost:5432/sqlalchemy_db", echo=True)
+
+meta = MetaData()
+
+people = Table(
+    "people",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("age", Integer)
+)
+
+meta.create_all(engine)
+
+conn = engine.connect()
+
+select_statement = people.select().where(people.c.age > 20)
+result = conn.execute(select_statement)
+
+for row in result.fetchall():
+    print(row)
